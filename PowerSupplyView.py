dbd456e5b047d8bc3da6abe8baf67d959fd4af13
Andrey Yakovlev <yakovlev-andreyka@yandex.ru>
2020-04-17 17:28:27 +0300

    Add UI to CV output and tracking functions.

--- PowerSupplyView.py
+++ PowerSupplyView.py
@@ -16,9 +16,13 @@ class PowerSupplyView(Frame):
 
         # CV output
         self.outputList = Combobox(self, height=30, width=5)
-        self.voltageEntry = Entry(self, width=10)
-        self.amperageEntry = Entry(self, width=10)
-        self.limitEntry = Entry(self, width=10)
+        self.voltageEntry = Entry(self, width=5)
+        self.amperageEntry = Entry(self, width=5)
+        self.limitEntry = Entry(self, width=5)
+        self.voltageList = Combobox(self, height=30, width=3)
+        self.amperageList = Combobox(self, height=30, width=3)
+        self.limitAmperageList = Combobox(self, height=30, width=3)
+        self.trackFunList = Combobox(self, height=30, width=5)
 
         self.initUI()
 
@@ -51,15 +55,41 @@ class PowerSupplyView(Frame):
 
         voltageLabel = Label(self, text='Enter voltage:')
         voltageLabel.place(x=2, y=275)
-        self.voltageEntry.place(x=120, y=270)
 
         amperageLabel = Label(self, text='Enter amperage:')
         amperageLabel.place(x=2, y=305)
-        self.amperageEntry.place(x=120, y=300)
 
         limitLabel = Label(self, text='Enter overcurrent\n protection limit:')
         limitLabel.place(x=2, y=335)
+
+        self.voltageEntry.place(x=120, y=270)
+        self.amperageEntry.place(x=120, y=300)
         self.limitEntry.place(x=120, y=340)
+
+        setCVButton = Button(self, text='Set CV output', command=self.clearTextField)
+        setCVButton.place(x=2, y=380)
+
+        self.voltageList.place(x=180, y=273)
+        self.voltageList['values'] = ('V', 'mV', 'uV', 'nV')
+        self.voltageList.current(0)
+
+        self.amperageList.place(x=180, y=303)
+        self.amperageList['values'] = ('A', 'mA', 'uA', 'nA')
+        self.amperageList.current(0)
+
+        self.limitAmperageList.place(x=180, y=343)
+        self.limitAmperageList['values'] = ('A', 'mA', 'uA', 'nA')
+        self.limitAmperageList.current(0)
+
+        # Setting track function
+        trackLabel = Label(self, text='Set track function\n to output:')
+        trackLabel.place(x=250, y=220)
+
+        self.trackFunList.place(x=250, y=260)
+        self.trackFunList['values'] = ('CH1', 'CH2')
+
+        trackButton = Button(self, text='Set', command=self.clearTextField)
+        trackButton.place(x=250, y=290)
 
     # Функция очистки всего поля с полученными сообщениями
     def clearTextField(self):
